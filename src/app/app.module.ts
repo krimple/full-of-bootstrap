c25fa5c9d2bc2a4aa343fb7f5862e5e6a2a928ac
Ken Rimple <krimple@chariotsolutions.com>
2017-04-24 08:23:19 -0400

    bootstrap configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NgbAlertModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NgbModule.forRoot(),
+    NgbAlertModule,
     BrowserModule,
     FormsModule,
     HttpModule
